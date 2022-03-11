d7dbd47c3349c34a7cc73256370e392c829272a7
Georgio El-Khoury <99497326+Amateurzz@users.noreply.github.com>
2022-03-11 20:26:47 +0200

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,35 @@
+package lab6;
+import java.util.Scanner;
+
+public class Employee {
+    // Initializing variables
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Employee() {
+        // Get user input
+        Scanner scan = new Scanner(System.in);
+
+        /**
+         * Continue code here to ask for the first name, last name, age, and salary, IN THAT ORDER
+         */System.out.println("Input the employee's first name:");
+         firstName = scan.nextLine();
+         System.out.println("Input the employee's second name:");
+         lastName = scan.nextLine();
+         scan.nextLine();
+         System.out.println("Input the employee's age");
+         age = scan.nextInt();
+         System.out.println("Input the employee's salary");
+         salary = scan.nextDouble();
+         
+         
+
+    }
+
+    public String toString() {
+        // insert logic for toString method
+    	return "Employee's information: "+firstName+" "+lastName+" "+age+" "+salary;
+    }
+}
